e1b7c159322c21ab79c697e51e8fe1990aadf4f9
EloiGry <eloi.grychta@gmail.com>
2021-09-27 13:31:15 +0200

    modif

---
+++ Jour1/index.js
@@ -0,0 +1,24 @@
+console.log("Hello world !")
+var test = "My name is Eloi"
+console.log(test)
+var firstName= "Eloi"
+console.log(`Nice to meet you ${firstName}`)
+var testLength = "I'm very long !"
+console.log(testLength.length)
+var food = "croissant is meh"
+food = food.replace("meh", "so good")
+console.log(food)
+var basic = "This is Cool"
+var basicUp = basic.toUpperCase()
+console.log(basicUp)
+var basicDown = basic.toLowerCase()
+console.log(basicDown)
+var word = "banana"
+var letters = word.split("")
+console.log(letters)
+var age = 25
+var template = `I'm ${age} years old`
+console.log(template)
+var changed = "bonjour"
+changed = changed.replace ("o", "a", "o", "a")
+console.log(changed)
