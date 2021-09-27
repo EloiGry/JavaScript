cbe423db74debd17a0c2ee20297fa7083fa9a0ed
EloiGry <eloi.grychta@gmail.com>
2021-09-27 17:32:36 +0200

    modif

--- Jour2/index.js
+++ Jour2/index.js
@@ -4,7 +4,7 @@ console.log(integer)
 console.log(float)
 
 var basic = 34
-var stringified = basic.toString
+var stringified = basic.toString()
 console.log(stringified)
 
 var num = 1.5
@@ -14,9 +14,99 @@ console.log(rounded)
 var test = 12
 var bis = 5
 console.log(test + bis)
-console.log(test + bis)
-console.log(test + bis)
-console.log(test + bis)
-console.log(test + bis)
-console.log(test + bis)
+console.log(test - bis)
+console.log(test * bis)
+console.log(test / bis)
+console.log(test ** bis)
+console.log(test % bis)
 
+var test = 143
+var bis = 219
+console.log(test > bis)
+console.log(test < bis)
+console.log(test >= bis)
+console.log(test <= bis)
+console.log(test == bis)
+console.log(test === bis)
+console.log(test != bis)
+console.log(test !== bis)
+
+var limit = 50
+var score = 64
+if (score >= limit) {
+    console.log("Ok good!")
+} else {
+    console.log("Oh noooo..")
+}
+
+var password = "azerty"
+if (password.length >= 5) {console.log("The password is secure")}
+
+if (score >= limit && password.length >= 5) {
+    console.log("Everything is good")
+} else if (score >= limit || password.length >= 5) {
+    console.log("Something is good")
+} else {
+    console.log("Nothing is good")
+
+}
+
+var random = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+if (random === 6) {
+    console.log("Yes I win!")
+}
+else {
+    console.log("So close...")
+}
+
+var month = "July"
+
+switch (month) {
+	case "March": 
+		console.log("Spring");
+		break;
+        case "May": 
+		console.log("Spring");
+		break;
+        case "April": 
+		console.log("Spring");
+		break;
+	    case "August": 
+		console.log("Summer");
+		break;
+        case "June": 
+		console.log("Summer");
+		break;
+        case "July": 
+		console.log("Summer");
+		break;
+	case "October": 
+		console.log("Fall");
+		break;
+        case "November": 
+		console.log("Fall");
+		break;
+        case "September": 
+		console.log("Fall");
+		break;
+        case "January": 
+		console.log("Winter");
+        break;
+        case "December": 
+		console.log("Winter");
+        break;
+        case "February": 
+		console.log("Winter");
+}
+
+var roundedNumber = 3.2
+var ceil = Math.ceil(roundedNumber) 
+var floor = Math.floor(roundedNumber)
+
+
+
+if (ceil - roundedNumber >= 0.5  ) {
+    console.log(Math.floor(roundedNumber))
+} else {
+    console.log(Math.ceil(roundedNumber))
+}
