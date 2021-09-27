d31d7170039059ffe522d44aa48cfa2813d9d5fb
EloiGry <eloi.grychta@gmail.com>
2021-09-27 15:44:37 +0200

    modif

---
+++ Jour2/index.js
@@ -0,0 +1,22 @@
+var integer = 102
+var float = 13.9
+console.log(integer)
+console.log(float)
+
+var basic = 34
+var stringified = basic.toString
+console.log(stringified)
+
+var num = 1.5
+var rounded = Math.round(num)
+console.log(rounded)
+
+var test = 12
+var bis = 5
+console.log(test + bis)
+console.log(test + bis)
+console.log(test + bis)
+console.log(test + bis)
+console.log(test + bis)
+console.log(test + bis)
+
