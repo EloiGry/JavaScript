81dff15d62a4ee6c86945dcbc3f42484c7d2bb50
EloiGry <eloi.grychta@gmail.com>
2021-09-28 17:27:26 +0200

    modif

--- Jour3/index.js
+++ Jour3/index.js
@@ -1,75 +1,96 @@
-var fruits = ["mango", "lemon", "blueberry"]
-console.log (fruits)
+// var fruits = ["mango", "lemon", "blueberry"]
+// console.log (fruits)
 
-console.table(fruits)
+// console.table(fruits)
 
-var ingredients = ["eggs", "milk", "butter"]
-console.log(ingredients[1])
-console.log(ingredients[2])
+// var ingredients = ["eggs", "milk", "butter"]
+// console.log(ingredients[1])
+// console.log(ingredients[2])
 
-var objects = ["pen", "book", "lamp"]
-objects.unshift("chair")
-console.log(objects)
-objects.pop()
-console.log(objects)
-objects.push("laptop")
-console.log(objects)
-objects.shift()
-console.log(objects)
+// var objects = ["pen", "book", "lamp"]
+// objects.unshift("chair")
+// console.log(objects)
+// objects.pop()
+// console.log(objects)
+// objects.push("laptop")
+// console.log(objects)
+// objects.shift()
+// console.log(objects)
 
-var numbers = [4, 10, 8, 12, 6]
-numbers.reverse()
-console.log(numbers)
-numbers.sort((a, b) => a - b)
-console.log(numbers)
-
-
-
-var total = 0
-var limit = 10
+// var numbers = [4, 10, 8, 12, 6]
+// numbers.reverse()
+// console.log(numbers)
+// numbers.sort((a, b) => a - b)
+// console.log(numbers)
 
 
-for (var i=0; i<=limit; i++) {
-    total = total + i
-}
-console.log(total)
+
+// var total = 0
+// var limit = 10
+
+
+// for (var i=0; i<=limit; i++) {
+//     total = total + i
+// }
+// console.log(total)
 
 
 // var sentence = "Hello Konexio!"
+// var knxo = sentence.split("")
+// console.log(knxo)
+
+// for (var i=knxo.length; i >= 0; i--){
+//     console.log(knxo[i])
+// }
 
 
-// for ( var i="Hello Konexio !"; 
 
 
 // Bonus
 
-for (var i=0; i<=100; i++) {
-    if(i % 3 === 0 && i!==0) {
-        console.log("fozz")
-    }
-    else if(i % 5 === 0 && i!==0) {
-        console.log("laz")
-    }
-    else if(i % 5 === 0 && i % 3 && i!==0) {
-        console.log("fozzlaz")
-    }
-    else if(i % 7 === 0 && i!==0) {
-        console.log(undefined)
-    }
-    else {
-        console.log(i)
-    }
-}
+// for (var i=0; i<=100; i++) {
+//     if(i % 3 === 0 && i!==0) {
+//         console.log("fozz")
+//     }
+//     else if(i % 5 === 0 && i!==0) {
+//         console.log("laz")
+//     }
+//     else if(i % 5 === 0 && i % 3 && i!==0) {
+//         console.log("fozzlaz")
+//     }
+//     else if(i % 7 === 0 && i!==0) {
+//         console.log(undefined)
+//     }
+//     else {
+//         console.log(i)
+//     }
+// }
 
 
 
+// Bonus 2
+// var total = 0
+// var limit = 10
 
 
+// while (limit < tota) {
+// 	total++;
+//     total = total + i
+// }
+
+// console.log(total)
 
 
+Bonus 3 
 
+var array = ["Hélène", "Jung", "Alin", "Kevin", "Ahmed", "Marco"]
 
+var min = 0;
+var max = 5;
 
+var aléatoire = Math.floor(Math.random() * (max - min + 1) + min);
+
+console.log
 
 
 
