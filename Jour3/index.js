5e48a1314af6fb8a59161d2bedaa7bd7e691d54d
EloiGry <eloi.grychta@gmail.com>
2021-09-29 11:51:31 +0200

    modif

--- Jour3/index.js
+++ Jour3/index.js
@@ -1,96 +1,112 @@
-// var fruits = ["mango", "lemon", "blueberry"]
-// console.log (fruits)
+var fruits = ["mango", "lemon", "blueberry"]
+console.log (fruits)
 
-// console.table(fruits)
+console.table(fruits)
 
-// var ingredients = ["eggs", "milk", "butter"]
-// console.log(ingredients[1])
-// console.log(ingredients[2])
+var ingredients = ["eggs", "milk", "butter"]
+console.log(ingredients[1])
+console.log(ingredients[2])
 
-// var objects = ["pen", "book", "lamp"]
-// objects.unshift("chair")
-// console.log(objects)
-// objects.pop()
-// console.log(objects)
-// objects.push("laptop")
-// console.log(objects)
-// objects.shift()
-// console.log(objects)
+var objects = ["pen", "book", "lamp"]
+objects.unshift("chair")
+console.log(objects)
+objects.pop()
+console.log(objects)
+objects.push("laptop")
+console.log(objects)
+objects.shift()
+console.log(objects)
 
-// var numbers = [4, 10, 8, 12, 6]
-// numbers.reverse()
-// console.log(numbers)
-// numbers.sort((a, b) => a - b)
-// console.log(numbers)
-
-
-
-// var total = 0
-// var limit = 10
-
-
-// for (var i=0; i<=limit; i++) {
-//     total = total + i
-// }
-// console.log(total)
-
-
-// var sentence = "Hello Konexio!"
-// var knxo = sentence.split("")
-// console.log(knxo)
-
-// for (var i=knxo.length; i >= 0; i--){
-//     console.log(knxo[i])
-// }
+var numbers = [4, 10, 8, 12, 6]
+numbers.reverse()
+console.log(numbers)
+numbers.sort((a, b) => a - b)
+console.log(numbers)
 
 
 
+var total = 0
+var limit = 10
 
-// Bonus
 
-// for (var i=0; i<=100; i++) {
-//     if(i % 3 === 0 && i!==0) {
-//         console.log("fozz")
-//     }
-//     else if(i % 5 === 0 && i!==0) {
-//         console.log("laz")
-//     }
-//     else if(i % 5 === 0 && i % 3 && i!==0) {
-//         console.log("fozzlaz")
-//     }
-//     else if(i % 7 === 0 && i!==0) {
-//         console.log(undefined)
-//     }
-//     else {
-//         console.log(i)
-//     }
-// }
+for (var i=0; i<=limit; i++) {
+    total = total + i
+}
+console.log(total)
+
+
+var sentence = "Hello Konexio!"
+var knxo = sentence.split("")
+var result = ""
+
+for (var i=knxo.length -1; i >= 0; i--){
+   result = result + knxo[i] 
+}
+console.log (result)
 
 
 
-// Bonus 2
-// var total = 0
-// var limit = 10
 
 
-// while (limit < tota) {
-// 	total++;
-//     total = total + i
-// }
 
-// console.log(total)
+Bonus
+
+for (var i=0; i<=100; i++) {
+    if(i % 3 === 0 && i!==0) {
+        console.log("fozz")
+    }
+    else if(i % 5 === 0 && i!==0) {
+        console.log("laz")
+    }
+    else if(i % 5 === 0 && i % 3 && i!==0) {
+        console.log("fozzlaz")
+    }
+    else if(i % 7 === 0 && i!==0) {
+        console.log(undefined)
+    }
+    else {
+        console.log(i)
+    }
+}
 
 
-Bonus 3 
 
-var array = ["Hélène", "Jung", "Alin", "Kevin", "Ahmed", "Marco"]
+Bonus 2
+var total = 0
+var limit = 10
+
+
+while (limit < tota) {
+	total++;
+    total = total + i
+}
+
+console.log(total)
+
+Bonus 3
+var array = ["Marco","Eloi","Jong","Hélène","Rébecca","Kevin JM","Kévin","Salwa","Hanaa","Ahmed","Alin","Jeremy","Vincent","Bilal","Edouard","Chaïma","Syrine","Séverin","Karimou","Evan"]
 
 var min = 0;
-var max = 5;
+var max = array.length - 1
 
-var aléatoire = Math.floor(Math.random() * (max - min + 1) + min);
+var random = Math.floor(Math.random() * (max - min + 1) + min);
 
-console.log
+console.log (array [random])
+
+var array = []
+var min = 0;
+var max = 100;
+
+for (var i=0; i < 20; i++) {
+    var random = Math.floor(Math.random() * (max - min + 1) + min);
+    array[i] = random
+}
+
+console.log(array)
+
+for (var i=0; i < array.length; i++) {
+    console.log(array[i])
+}
 
 
 
