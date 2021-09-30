1d79fcf6129d13e7c5bbbfcacebd153e2b25c2f1
EloiGry <eloi.grychta@gmail.com>
2021-09-30 11:00:17 +0200

    job done

--- Jour3V2/index.js
+++ Jour3V2/index.js
@@ -127,3 +127,14 @@
 
 // Amstrong
 
+
+
+for (i=0; i<10; i++) {
+    for (j=0; j<10; j++ ) {
+    for (k=0; k<10; k++ ) { 
+        var result = i * 100 + j*10 + k
+        var total = i**3 + j**3 + k**3
+     if (total === result) {console.log(total)}
+    }
+}   
+}
